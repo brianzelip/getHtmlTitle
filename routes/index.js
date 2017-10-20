b85d60f946ca29ebe379bdb4ad8d7329d9235056
Brian Zelip <bzelip@gmail.com>
2017-10-20 08:03:36 -0400

    minor formatting to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const titleController = require('../controllers/titleController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', titleController.homePage);
+router.get('/', catchErrors(titleController.homePage));
 
 module.exports = router;
