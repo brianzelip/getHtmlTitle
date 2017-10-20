13887664d900cebe29538f530c62ee4a5ed3e555
Brian Zelip <bzelip@gmail.com>
2017-10-20 08:04:14 -0400

    get working async function that returns an html doc title

--- controllers/titleController.js
+++ controllers/titleController.js
@@ -1,15 +1,27 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-exports.homePage = (req, res) => {
-  axios
-    .get('http://zelip.me')
-    .then(function(response) {
-      const $ = cheerio.load(response.data);
-      console.log('here comes the title text...', $('title').text());
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+async function getHtmlTitle(url) {
+  const response = await axios.get(url);
+  const html = await cheerio.load(response.data);
+  const title = await html('title').text();
+  // console.log('response', response);
+  console.log('title', title);
+
+  return title;
+}
+
+exports.homePage = async (req, res) => {
+  const title = await getHtmlTitle('http://zelip.me');
+  console.log('homePage says, title is: ', title);
+  // axios
+  //   .get('http://zelip.me')
+  //   .then(function(response) {
+  //     const $ = cheerio.load(response.data);
+  //     console.log('here comes the title text...', $('title').text());
+  //   })
+  //   .catch(function(error) {
+  //     console.log(error);
+  //   });
   res.render('index');
 };
