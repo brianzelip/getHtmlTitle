e07e5101fe516071ccebb0d2a39c5c486ae4fb82
Brian Zelip <bzelip@gmail.com>
2017-10-20 05:52:47 -0400

    install cheerio for parsing html to get title, successfully getting title 🎉

--- controllers/titleController.js
+++ controllers/titleController.js
@@ -1,11 +1,13 @@
 const request = require('request');
 const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 
 exports.homePage = (req, res) => {
-  console.log('hi there!');
-  const title = fetch('http://zelip.me').then(data => {
-    console.log(data);
+  request('http://zelip.me', function(error, response, body) {
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    const $ = cheerio.load(body);
+    console.log('here comes the title text...', $('title').text());
   });
-  console.log("this prolly won't be at the end!");
   res.render('index');
 };
