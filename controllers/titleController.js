d73bed82867769bc6255dcca8ff38e7c89bd957a
Brian Zelip <bzelip@gmail.com>
2017-10-20 06:15:20 -0400

    swap axios for request in getting the title of the html doc

--- controllers/titleController.js
+++ controllers/titleController.js
@@ -1,13 +1,15 @@
+const axios = require('axios');
 const cheerio = require('cheerio');
-const fetch = require('node-fetch');
-const request = require('request');
 
 exports.homePage = (req, res) => {
-  request('http://zelip.me', function(error, response, body) {
-    console.log('error:', error);
-    console.log('statusCode:', response && response.statusCode);
-    const $ = cheerio.load(body);
-    console.log('here comes the title text...', $('title').text());
-  });
+  axios
+    .get('http://zelip.me')
+    .then(function(response) {
+      const $ = cheerio.load(response.data);
+      console.log('here comes the title text...', $('title').text());
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   res.render('index');
 };
